3b60057c3540c281ce62cc9342cb8de3f2184106
faiz borkar <faiz.borkar@neosoftmail.com>
2021-01-15 18:48:00 +0530

    basic jwt authentication has been completed

---
+++ user/api/v1/urls.py
@@ -0,0 +1,27 @@
+from django.urls import path
+
+from rest_framework_simplejwt import views as jwt_views
+
+from .views import SignUpAPIView, ListUsers
+
+urlpatterns = [
+    path(
+        'signup/',
+        SignUpAPIView.as_view(),
+    ),
+    path(
+        'token/',
+        jwt_views.TokenObtainPairView.as_view(),
+        name='token_obtain_pair'
+    ),
+    path(
+        'token/refresh/',
+        jwt_views.TokenRefreshView.as_view(),
+        name='token_refresh'),
+    path(
+        'users/',
+        ListUsers.as_view(),
+        name='list_users',
+    )
+
+]
---
+++ user/api/v1/views.py
@@ -0,0 +1,33 @@
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import SignUpSerializer
+from rest_framework_simplejwt.authentication import JWTAuthentication, JWTTokenUserAuthentication
+
+from django.contrib.auth.models import User
+
+class SignUpAPIView(generics.GenericAPIView):
+    serializer_class = SignUpSerializer
+    
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        message = {'message': f'{user.username} registered successfully'}
+        return Response(
+            message,
+            status=status.HTTP_201_CREATED
+        )
+
+from rest_framework.views import APIView
+
+from rest_framework.permissions import IsAuthenticated
+
+class ListUsers(APIView):
+    permission_classes = (IsAuthenticated,)
+    def get(self, request, format=None):
+        """
+        Return a list of all users.
+        """
+        usernames = [user.username for user in User.objects.all()]
+        return Response(usernames)
