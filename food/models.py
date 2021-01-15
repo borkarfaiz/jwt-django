946862acb93cfa5216b84083b8398291008b0854
faiz borkar <faiz.borkar@neosoftmail.com>
2021-01-15 18:55:53 +0530

    food app added

---
+++ food/models.py
@@ -0,0 +1,8 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Food(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
